f7ccc53db7fe6744f5f306a45edc8a01e5b5607d
Joshua Huston <joshua.david.huston@gmail.com>
2021-10-28 13:33:10 -0400

    Lab 12 backend Post Delete

--- modules/seedBook.js
+++ modules/seedBook.js
@@ -5,15 +5,15 @@ const BookModel = require('./book');
 function seedBook(req, res) {
   //Sample data entry
   const seedArr = [
-    {  title: 'Moby Dick', description: 'Hunted Whale', status: 'CHECKEDIN', email: 'someemail@gmail.com' },
-    {title: 'Pride and Prejudice', description: 'Elizabeth <3 Darcy', status: 'CHECKEDIN', email: 'someotheremail@gmail.com'},
-    {title: 'Les Miserables', description: 'Miserable French People', status: 'CHECKEDIN', email: 'yetanotheremail@gmail.com'}
+    { title: 'Moby Dick', description: 'Hunted Whale', status: 'CHECKEDIN', email: 'someemail@gmail.com' },
+    { title: 'Pride and Prejudice', description: 'Elizabeth <3 Darcy', status: 'CHECKEDIN', email: 'someotheremail@gmail.com' },
+    { title: 'Les Miserables', description: 'Miserable French People', status: 'CHECKEDIN', email: 'yetanotheremail@gmail.com' }
   ]
   seedArr.forEach(seed => {
     let entry = new BookModel(seed);
     entry.save();
   });
-  
+
   res.status(200).send('Seeded Database');
 }
 
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 const app = express();
 const seedBook = require('./modules/seedBook.js');
 app.use(cors());
+app.use(express.json());
 const BookModel = require('./modules/book.js');
 const bombTheBase = require('./modules/clear');
 
@@ -19,17 +20,27 @@ app.get('/', (request, response) => {
   response.status(200).send('Welcome to server');
 })
 
+app.get('/test', (req, res) => res.status(200).send('Test Complete'));
+app.post('/test/:info', (req, res) => {
+  console.log('\nQuery:', req.query, '\nParams:', req.params, '\nBody:', req.body);
+  res.status(200).send('Posted');
+})
+
 app.get('/seed', seedBook);
 app.get('/clear', bombTheBase);
+app.delete('/allBooks/:id', deleteBook);
 
 app.get('/allBooks', (req, res) => {
   BookModel.find((err, item) => {
     if (err) return res.status(500).send(err);
     else {
-    res.status(200).send(item);
-  }
+      res.status(200).send(item);
+    }
   });
 });
+
+// Lab 12
+app.post('/allBooks', postBook);
 
 app.get('*', (request, response) => {
   response.status(500).send('Route not found');
@@ -49,6 +60,34 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', _ => {
   console.log('We\'re connected!');
 });
+
+
+async function postBook(req, res) {
+
+  let postObj = req.body
+  try {
+    let postEntry = BookModel(postObj);
+    postEntry.save();
+
+    res.status(200).send(postObj);
+  }
+  catch {
+    res.status(500).send('error:', err.message);
+  }
+
+}
+
+async function deleteBook(req, res) {
+  let { id } = req.params;
+  console.log(id);
+  try {
+    let deletedObj = await BookModel.findByIdAndDelete(id);
+    res.status(200).send(deletedObj);
+  }
+  catch {
+    res.status(500).send('error:', err.message);
+  }
+}
 
 
 // Handle Errors
